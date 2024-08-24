11d3944d29483999aaa1fc4939f9f1986f5884ed
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-08-23 20:05:57 -0700

    Update Selective-array-reversing.js

--- Selective-array-reversing.js
+++ Selective-array-reversing.js
@@ -24,3 +24,12 @@ Note:
 
 
 // Solution
+
+function selReverse(array, length) {
+  if (length == 0) return array;
+  let result = [];
+  for(let i = 0; i < array.length; i += length) {
+    result.push(...array.slice(i, i+length).reverse());
+  }
+  return result;
+}
